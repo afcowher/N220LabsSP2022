a687f44982b3afaa7eb83cc74e3ab2d77b2fc0e3
afcowher <afcowher@hotmail.com>
2022-01-15 20:26:53 -0500

    Chapter 1-3 examples

---
+++ Book Exercises/Chp3Ex3-2/js/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(480, 120);
+}
+
+function draw() {
+  background(204);
+  point(0, 0);
+  point(1, 1);
+  point(1, 2);
+  point(1, 3);
+  point(1, 4);
+  point(1, 5);
+  point(1, 6);
+  point(1, 7);
+  point(1, 8);
+  point(1, 9);
+  point(1, 10);
+  point(479, 0);
+  point(479, 119)
+  point(0, 119)
+  point(240, 60);
+}
