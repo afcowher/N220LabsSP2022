7683f6fffac9679e23881fb99391805fd5cf0472
afcowher <afcowher@hotmail.com>
2022-01-18 11:36:05 -0500

    1/18/2022 Class exercises

---
+++ Class Exercises/Jan18-1/js/app.js
@@ -0,0 +1,8 @@
+function setup(){
+    createCanvas(800,600);
+
+    circle(150, 150, 40);
+    circle(300, 150, 40);
+
+    arc(200, 300, 200, 200, 0, PI, CHORD);
+}
---
+++ Class Exercises/Jan18-2/js/app.js
@@ -0,0 +1,13 @@
+function setup(){
+    createCanvas(800,600);
+
+   stroke(255, 255, 255);
+   strokeWeight(10);
+
+   fill(255, 99, 99);
+   circle(150, 150, 200);
+   fill(186, 255, 180);
+   circle(150, 150, 150);
+   fill(255, 171, 118);
+   circle(150, 150, 50);
+}
