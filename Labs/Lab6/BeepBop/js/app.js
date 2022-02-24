b29405b6493a97f6cea5f9c2527c150ee39ef61f
afcowher <afcowher@hotmail.com>
2022-02-24 12:55:04 -0500

    Finished Lab 6

--- Labs/Lab6/BeepBop/js/app.js
+++ Labs/Lab6/BeepBop/js/app.js
@@ -1,11 +1,22 @@
 //Adam Cowher
-//2/23/2022
+//2/24/2022
 //N220
 
 //retrieves the div with id "beep"
 let beepDiv = document.getElementById("beep");
 
-for (let i = 1; i <= 25; i++){
-    //prints the last number to the end of the div
-    beepDiv.append(i + ", ");
+//loop that iterates through the first 25 numbers
+for (let i = 1; i <= 25; i++) {
+    //if the iterand is divisible by 3 and 5 - beepbop
+    //if the iterand is divisible by just 3 - beep
+    //if the iterand is divisible by just 5 - bop
+    if (i % 3 == 0 && i % 5 == 0) {
+        beepDiv.append("beepbop, ")
+    } else if (i % 3 == 0) {
+        beepDiv.append("beep, ");
+    } else if (i % 5 == 0) {
+        beepDiv.append("bop, ");
+    } else {
+        beepDiv.append(i + ", ");
+    }
 }
