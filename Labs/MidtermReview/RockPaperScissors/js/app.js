ce65d7428fd25083256086d818c2f84a0512f094
afcowher <afcowher@hotmail.com>
2022-03-03 18:06:24 -0500

    finished up midterm exercises + refined index

--- Labs/MidtermReview/RockPaperScissors/js/app.js
+++ Labs/MidtermReview/RockPaperScissors/js/app.js
@@ -2,18 +2,26 @@
 //3/3/2022
 //N220
 
+//global variable to store the score
 var score = 0;
 
+//array that stores the CPU's options
 var options = ["Rock", "Paper", "Scissors"];
 
+//accesses the score div and writes the initial score
 var scoreDiv = document.getElementById("scoreDiv");
 scoreDiv.innerHTML = "Score: " + score;
 
+//accesses the results div
 var resultsDiv = document.getElementById("resultsDiv");
 
+//accesses the rock div and labels it as "Rock"
 var rockDiv = document.getElementById("rockDiv");
 rockDiv.innerHTML = "Rock";
 
+//function that initiates when user selects "Rock" - generates a random selection for the CPU, and
+//utilizes the RPS logic when necessary. Displays the results in the resultsDiv, and updates the score
+//inside the scoreDiv
 function playRock(){
     let cpuMove = options[Math.floor(Math.random() * options.length)];
     if (cpuMove == "Scissors"){
@@ -26,9 +34,13 @@ function playRock(){
     scoreDiv.innerHTML = "Score: " + score;
 }
 
+//accesses the paper div and labels it as "Paper"
 var paperDiv = document.getElementById("paperDiv");
 paperDiv.innerHTML = "Paper";
 
+//function that initiates when user selects "Paper" - generates a random selection for the CPU, and
+//utilizes the RPS logic when necessary. Displays the results in the resultsDiv, and updates the score
+//inside the scoreDiv
 function playPaper(){
     let cpuMove = options[Math.floor(Math.random() * options.length)];
     if (cpuMove == "Rock"){
@@ -41,9 +53,13 @@ function playPaper(){
     scoreDiv.innerHTML = "Score: " + score;
 }
 
+//accesses the scissors div and labels it as "Scissors"
 var scissorsDiv = document.getElementById("scissorsDiv");
 scissorsDiv.innerHTML = "Scissors";
 
+//function that initiates when user selects "Scissors" - generates a random selection for the CPU, and
+//utilizes the RPS logic when necessary. Displays the results in the resultsDiv, and updates the score
+//inside the scoreDiv
 function playScissors(){
     let cpuMove = options[Math.floor(Math.random() * options.length)];
     if (cpuMove == "Rock"){
@@ -56,9 +72,13 @@ function playScissors(){
     scoreDiv.innerHTML = "Score: " + score;
 }
 
+//accesses the guard div and labels it as "Guard"
 var guardDiv = document.getElementById("guardDiv");
 guardDiv.innerHTML = "Guard";
 
+//function that initiates when user selects "Guard" - generates a random selection for the CPU, and
+//subtracts 0.5 from the user's score. Displays the results in the resultsDiv, and updates the score
+//inside the scoreDiv
 function playGuard(){
     score = score - 0.5;
     let cpuMove = options[Math.floor(Math.random() * options.length)];
